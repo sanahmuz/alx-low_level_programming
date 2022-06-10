80c72c0221a12782eb21a2c15edd0168cf3d0119
jefferson kimani <jeffgkim56@gmail.com>
2022-06-10 01:03:39 -0700

    update 9*

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,11 +5,11 @@
  */
 void times_table(void)
 {
-	int num, mult, prod;
+	int num;
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar('0')
+		_putchar('0');
 
 		for (mult = 1; mult <= 9; mult++)
 		{
