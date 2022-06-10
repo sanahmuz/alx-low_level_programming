d3536900939e14d0a0a0cd2331781c2ba8696e2d
jefferson kimani <jeffgkim56@gmail.com>
2022-06-10 00:32:43 -0700

    Update abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Computes absolute val of integer.
+ * @n: interger to be computed.
+ *
+ * Return: The absolute val of interger.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
