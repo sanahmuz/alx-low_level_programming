419686db016e2de847efb405633c64ae9920ef8f
jefferson kimani <jeffgkim56@gmail.com>
2022-06-09 23:29:53 -0700

    Update put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,35 +1,19 @@
 #include "main.h"
-
 /**
-<<<<<<< HEAD
- * main - Print "-putchar" followed by line.
-=======
- * main - Prints _putchar followed by a new line.
->>>>>>> refs/remotes/origin/main
- *
- * Return: Always 0.
+ * main - my entry point
+ * Description: program that prints _putchar
+ * Return: 0
  */
 int main(void)
 {
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
-<<<<<<< HEAD
 	_putchar('t');
-=======
-	_putchar('t'');
->>>>>>> refs/remotes/origin/main
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-<<<<<<< HEAD
 	_putchar('\n');
-	
-	return (0)\t;
-}	
-=======
-
 	return (0);
 }
->>>>>>> refs/remotes/origin/main
