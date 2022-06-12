532480ef63b3ab2e0aae040013b8505827987171
jefferson kimani <jeffgkim56@gmail.com>
2022-06-12 09:06:10 -0700

    Update 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a traingle, using the #.
+ * @size: The size of triangle.
+ */
+void print_triangle(int size)
+{
+	int hash, index;
+
+	if (size > 0)
+	{
+		for (hash = 1; hash <= size; hash++)
+		{
+			for (index = size - hash; index > 0; index--)
+				_putchar(' ');
+
+			for (index = 0; index < hash; index++)
+				_putchar('#');
+
+			if (hash == size)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
