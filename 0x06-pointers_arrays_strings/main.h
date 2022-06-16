62e304be0bf048e69129a09a3d1dbbd2b6f4815d
jefferson kimani <jeffgkim56@gmail.com>
2022-06-16 15:31:06 -0700

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef main_h
+#define main_h
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -10,3 +13,5 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+#endif
