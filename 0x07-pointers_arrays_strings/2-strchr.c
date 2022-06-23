e9468a8a3ded06b16e5e063b5a9efe2722126cae
jefferson kimani <jeffgkim56@gmail.com>
2022-06-23 02:42:10 -0700

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,25 +1,20 @@
 #include "main.h"
-
 /**
- * *_strchr - prints found c
- * @s: pointer to char
- * @c: char params to be found
- * Return: *S
+ * *_strchr - locates a character in a string.
+ * @s: char 1 pointer
+ * @c: char s
+ * Return: '\0' or s
  */
-
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int a;
+
+	for (a = 0; s[a] >= '\0'; a++)
 	{
-		if (*s == c)
+		if (s[a] == c)
 		{
-			return (s);
+			return (s + a);
 		}
-		s++;
 	}
-	if (*s == c)
-	{
-		return (s);
-	}
-return (0);
+	return ('\0');
 }
